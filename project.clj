c36da7c2e0d39fd08d8a7004fc7ff3445862ef76
Mike Easter <michael.easter@qg.com>
2017-06-20 16:36:01 -0500

    Fixes.

--- project.clj
+++ project.clj
@@ -15,32 +15,31 @@
                  [day8.re-frame/http-fx "0.1.3"]
                  [cljs-ajax "0.6.0"]]
   :plugins [[lein-cljsbuild "1.1.5"]
-            [cider/cider-nrepl "0.14.0"]
-            [org.clojure/tools.nrepl "0.2.12"]
+            [lein-doo "0.1.7"]
             [lein-figwheel "0.5.10"]]
   :source-paths ["src/cljs"]
   :hooks [leiningen.cljsbuild]
   :profiles {:dev {:source-paths ["dev"]
                    :target-path "target/dev"
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [com.cemerick/piggieback "0.2.2"]
-                                  [figwheel-sidecar "0.5.10"]]
+                   :dependencies [[com.cemerick/piggieback "0.2.2"]
+                                  [figwheel-sidecar "0.5.10"]
+                                  [org.clojure/tools.namespace "0.2.11"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
-  :cljsbuild {:test {:source-paths ["src" "test"]
-                     :compiler {:output-to     "target/testable.js"
-                                :main          'frog.runner
-                                :optimizations :simple}}
-              :builds {:dev {:source-paths ["src"]
-                             :figwheel {:on-jsload "frog.core/run"}
-                             :compiler {:output-to "resources/public/js/client.js"
-                                        :main "frog.core"
-                                        :asset-path "js"
-                                        :optimizations :none
-                                        :source-map true
-                                        :source-map-timestamp true}}}}
+  :cljsbuild {:builds {:test {:source-paths ["src" "test"]
+                              :compiler {:output-to     "target/testable.js"
+                                         :main          'frog.runner
+                                         :optimizations :simple}}
+                       :builds {:dev {:source-paths ["src"]
+                                      :figwheel {:on-jsload "frog.core/run"}
+                                      :compiler {:output-to "resources/public/js/client.js"
+                                                 :main "frog.core"
+                                                 :asset-path "js"
+                                                 :optimizations :none
+                                                 :source-map true
+                                                 :source-map-timestamp true}}}}}
   :doo {:build "test"
         :paths {:slimer "./node_modules/.bin/slimerjs"}
         :alias {:default [:slimer]
                 :browsers [:chrome :firefox]
                 :all [:browsers :headless]}}
-  :clean-targets ^{:protect false} ["resources/public/js"]})
+  :clean-targets ^{:protect false} ["resources/public/js"])
